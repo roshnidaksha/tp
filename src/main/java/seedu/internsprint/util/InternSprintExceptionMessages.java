8b974633c7eb610333501c07f825fb23da2e5b51
roshnidaksha <groshnidaksha2@yahoo.com>
2025-03-08 19:06:44 +0800

    Add JUnit tests for Parser class

--- src/main/java/seedu/internsprint/util/InternSprintExceptionMessages.java
+++ src/main/java/seedu/internsprint/util/InternSprintExceptionMessages.java
@@ -1,6 +1,7 @@
 package seedu.internsprint.util;
 
 public class InternSprintExceptionMessages {
-    public static final String ILLEGAL_COMMAND_INPUT = "Illegal input: Unexpected '/' found in value for key";
-    public static final String MISSING_COMMAND_INPUT = "Invalid input: key /%s found with no value.";
+    public static final String ILLEGAL_VALUE_INPUT = "Illegal input: Unexpected '/' found in value for key";
+    public static final String MISSING_VALUE_INPUT = "Invalid input: key %s found with no value.";
+    public static final String INVALID_FLAG_INPUT = "Invalid flag input: Flag must start with '/'";
 }
