9c7a2e1aed7bb83bd0af1745bdeaa42b26ce0fae
nmarwah7 <marwahnikitalinkedin@gmail.com>
2025-03-16 12:19:37 +0800

    Create a user profile class with a few parameters
    There is no way to store the data of a user such as their preferred
    roles and industries. Let's set up the architecture to allow this
    to be expanded on for later versions.

---
+++ src/main/java/seedu/internsprint/userProfile/UserProfile.java
@@ -0,0 +1,15 @@
+package seedu.internsprint.userProfile;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class UserProfile {
+    public static String name;
+    public static String yearlyGoals;
+    public static String monthlyTarget;
+    public static ArrayList<String> preferredIndustries;
+    public static ArrayList<String> preferredCompanies;
+    public static ArrayList<String> preferredRoles;
+    public static String targetStipendRange;
+    public static LocalDateTime[] internshipDateRange;
+}
